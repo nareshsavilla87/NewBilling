b25fbe037f2f6a475c16e8fbc8a8dc878cfec5dc
mujeeb1238 <mujeeb1238@gmail.com>
2014-09-19 16:26:02 +0530

    global url added in selfcare that is any url its working fine

--- myaccount/selfcare_module/selfcare_models/returnURL.js
+++ myaccount/selfcare_module/selfcare_models/returnURL.js
@@ -4,14 +4,15 @@
 	//console.log(hostName);
 	var locationOrigin = window.location.origin;
 	console.log(locationOrigin);
-	//var locationPathname = window.location.pathname;
+	var locationPathname = window.location.pathname;
+	console.log(locationPathname);
 	 
    selfcare.models = _.extend(selfcare_module, {
 	   //return URL form after clients success
 	   //returnURL : "https://"+hostName+"/Clientapp/myaccount/index.html#/active",
 	   //selfcareAppUrl : "https://"+hostName+"/Clientapp/myaccount/index.html"
-	   returnURL : locationOrigin+"/Clientapp/myaccount/index.html#/active",
-	   selfcareAppUrl : locationOrigin+"/Clientapp/myaccount/index.html"
+	   returnURL : locationOrigin+locationPathname+"#/active",
+	   selfcareAppUrl : locationOrigin+""+locationPathname
 		   
   });
 }(selfcare.models || {}));
