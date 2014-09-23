cce6abdafe84ed6b31619bd12965decf6af2f6ba
mujeeb1238 <mujeeb1238@gmail.com>
2014-09-23 21:58:34 +0530

    dalpay return url added

--- myaccount/selfcare_module/selfcare_controllers/main/AdditionalOrdersController.js
+++ myaccount/selfcare_module/selfcare_controllers/main/AdditionalOrdersController.js
@@ -61,7 +61,7 @@
 		    	  if(paymentGatewayName == 'dalpay'){
 		    		  scope.paymentDalpayURL = scope.dalpayURL+"&cust_name="+scope.formData.lastname+"&cust_phone="+scope.formData.phone+"&cust_email="+scope.formData.email+"&cust_state="+scope.formData.state+""+
   	  				"&cust_address1="+scope.formData.addressNo+"&cust_zip="+scope.formData.zip+"&cust_city="+scope.formData.state+"&item1_desc="+scope.formData.planName+"&item1_price="+scope.formData.planAmount+"" +
-  	  				"&user1="+scope.formData.id+"&user3=additionalorderspreviewscreen/"+routeParams.orderId+"/"+routeParams.clientId; 
+  	  				"&user1="+scope.formData.id+"&user2="+hostName+"&user3=additionalorderspreviewscreen/"+routeParams.orderId+"/"+routeParams.clientId; 
 		    	  }else if(paymentGatewayName == 'korta'){
 		    		  var token = selfcareUserData.token;
 		    		  if(token != null && token != ""){
