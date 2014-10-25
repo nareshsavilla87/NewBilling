666c9e405a547de400ec1f16f8646dfab4abb580
mujeeb1238 <mujeeb1238@gmail.com>
2014-10-25 14:24:47 +0530

    transaction history added

--- app/scripts/filters/EventTypeCategory.js
+++ app/scripts/filters/EventTypeCategory.js
@@ -3,14 +3,37 @@
 	  EventTypeCategory: function () {
                     return function(history,val) {
                     		
+                    	var jsonStringData = history;
+                    	var jsonArray = JSON.parse(jsonStringData);
                       var  eventTypeCategoryMsg = {
-                    		  
+                    	
+                        //client related data
                         "CREATE CLIENT" : "Client Created Succesfully" ,
-                        "CREATE ONETIMESALE" : "Item Sale Added Succesfully",
-                        "DEALLOCATE INVENTORY" : "Deallocated item",
-                        "CREATE ORDER" : "Order Created successfully",
+                        "UPDATE CLIENT" : "Client Updated",
+                        
+                        
+                       //order related data 
+                        "CREATE ORDER" : "Order Created successfully for "+jsonArray.paytermCode+" with plan Code "+jsonArray.planCode,
                         "DELETE ORDER" : "Order Deleted successfully",
+                        "RECONNECT ORDER" : "Order Re-Connected successfully",
+                        "REACTIVE ORDER" : "Order Re-Actived successfully",
+                        "SUSPEND ORDER" : "Order Suspended with reason "+jsonArray.suspensionReason+" and Description "+jsonArray.suspensionDescription,
+                        "RETRACKOSDMESSAGE ORDER" : "Order Re-Track OSD Message Added with commandName "+jsonArray.commandName,
+                        "APPLYPROMO ORDER" : "Apply Promo Added for Order With Promo Id "+jsonArray.promoId,
+                        "EXTENSION ORDER" : "Order Extended to "+jsonArray.extensionPeriod+" Due To "+jsonArray.extensionReason,
+                        
+                        //payments related Data
                         "CREATE PAYMENT" : "Payment Created successfully",
+                        "CANCEL PAYMENT" : "Payment Canceled Succesfully with remarks "+jsonArray.cancelRemark,
+                        
+                        //itemsale related data
+                        "CREATE ONETIMESALE" : "Item Sale Created Succesfully with Charge Code "+jsonArray.chargeCode+" ,UnitPrice "+jsonArray.unitPrice+"" +
+                        						" ,Total Price "+jsonArray.totalPrice+" ,Quantity "+jsonArray.quantity+" and SaleType "+jsonArray.saleType,
+                        "DEALLOCATE INVENTORY" : "Deallocated item",
+                        
+                        
+                        
+                        
                         "CREATE ADJUSTMENT" : "Adjustmented successfully",
                         "CREATE TICKET" : "Ticket Created Succesfully",
                         "CREATE CLIENTIDENTIFIER" : "Client Identifier Added Succesfully",
@@ -21,6 +44,13 @@
                         "UPDATE Client_Contact_Info" : "Client Contact Information Updated Succesfully",
                         "DELETE Client_Contact_Info" : "Client Contact Information Deleted Succesfully",
                         "CLOSE TICKET" : "Ticket Closed Succesfully",
+                        "CREATE OWNEDHARDWARE" : "Owned Hardware Created",
+                        "CREATE ALLOCATION" : "Allocation Added Succesfully",
+                        "TERMINATE ORDER" : "Order Terminated",
+                        
+                        
+                        
+                        
                         "SELFREGISTRATION ACTIVATE" : "Selfcare Registration Activated",
                         "CREATE SELFCARE" : "Selfcare Account Created Succesfully",
                         
