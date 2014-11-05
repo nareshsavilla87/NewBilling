7853c0a7df204758bebf758a69dc3656d1c83a80
mujeeb1238 <mujeeb1238@gmail.com>
2014-11-05 17:33:37 +0530

    auto completed errors fixed in all screens like ipchange,second sale,new sale,device swap

--- app/scripts/controllers/organization/OfficeAdjustmentsController.js
+++ app/scripts/controllers/organization/OfficeAdjustmentsController.js
@@ -11,7 +11,6 @@
         resourceFactory.officeAdjustmentsTemplateResource.getAdjustments(function(data){
           scope.discountOptions = data.discountOptions;
           scope.data = data.data;
-          scope.formData.adjustment_type = scope.discountOptions[0].discountType;
         });
         
         scope.submit = function() {
