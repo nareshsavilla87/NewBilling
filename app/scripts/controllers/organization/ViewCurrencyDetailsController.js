103d9a371852771acff2620205373de96e725475
Ranjith15 <ranjithworld150@gmail.com>
2014-11-24 14:30:38 +0530

    ..

--- app/scripts/controllers/organization/ViewCurrencyDetailsController.js
+++ app/scripts/controllers/organization/ViewCurrencyDetailsController.js
@@ -1,25 +1,49 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-	  ViewCurrencyDetailsController: function(scope, routeParams , route, location, resourceFactory, http,PermissionService) {
+	  ViewCurrencyDetailsController: function(scope, routeParams , route, location, resourceFactory, http,PermissionService, $modal) {
 		
         scope.viewcurrencydetails=[];
         scope.PermissionService = PermissionService;
         
-        resourceFactory.currencyResource.getCurrencyDetails({id: routeParams.id} , function(data) {
+        resourceFactory.currencyResource.get({id: routeParams.id} , function(data) {
             scope.viewcurrencydetails = data; 
             
         });
-        scope.deletemessage = function (){
-            resourceFactory.currencyResource.delete({id: routeParams.id} , {} , function(data) {
-                  location.path('/currencydetails');
-                  // added dummy request param because Content-Type header
-					// gets removed
-                  // if the request does not contain any data (a request body)
-            });
-          };
+        
+        /**
+    	 * Delete currency details 
+    	 * */
+      scope.deleteCurrency = function (){
+       	 $modal.open({
+	                templateUrl: 'deleteCurrency.html',
+	                controller: approve,
+	                resolve:{}
+	        });
+       };
+       
+   	function  approve($scope, $modalInstance) {
+   		$scope.approve = function () {
+   			resourceFactory.currencyResource.remove({id: routeParams.id} , {} , function(data) {
+   			 location.path('/currencydetails');
+           });
+           	 $modalInstance.dismiss('delete');
+        };
+           $scope.cancel = function () {
+               $modalInstance.dismiss('cancel');
+         };
+       }     
     }
   });
-  mifosX.ng.application.controller('ViewCurrencyDetailsController', ['$scope', '$routeParams', '$route', '$location', 'ResourceFactory', '$http','PermissionService', mifosX.controllers.ViewCurrencyDetailsController]).run(function($log) {
+  mifosX.ng.application.controller('ViewCurrencyDetailsController', [
+   '$scope', 
+   '$routeParams', 
+   '$route', 
+   '$location', 
+   'ResourceFactory', 
+   '$http',
+   'PermissionService',
+   '$modal',
+   mifosX.controllers.ViewCurrencyDetailsController]).run(function($log) {
     $log.info("ViewCurrencyDetailsController initialized");
   });
 }(mifosX.controllers || {}));
