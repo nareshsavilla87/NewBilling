33815d92be85dcd9d2bdf65a21550af7220d3550
Rakesh <rakesh.adlagatta@gmail.com>
2014-11-28 15:03:27 +0530

    configlookup tab added

--- app/scripts/filters/ConfigLookup.js
+++ app/scripts/filters/ConfigLookup.js
@@ -2,22 +2,37 @@
     mifosX.filters = _.extend(module, {
         ConfigLookup: function ($http) {
         	
+        	var paymentValue = "true";
+        	var ipTvValue = "false";
+        	var isClientIndividualValue = "false";
+        	var deviceAgrementTypeValue = "SALE";
         	
             return function(input) {    
             	
             	var  configNameLookup = {
             			
                    //For Enabling payment details in Simple Activation
-                   "payment":"true",
+                   "payment":paymentValue,
                   
                    //For enabling Category Details in Service Mapping For IPTV
-                   "IPTV":"false",
+                   "IPTV":ipTvValue,
                   
                    //Represent the Default Client Type IND or Corporate in Create Client 
-                   "IsClientIndividual":"false"
+                   "IsClientIndividual":isClientIndividualValue,
+                   
+                   "deviceAgrementType":deviceAgrementTypeValue
                 	 
                  };
-                  return configNameLookup[input];};
+            	if(input == 'jsonData'){
+            		return [{"name":"payment","value":paymentValue},
+            		        {"name":"IPTV","value":ipTvValue},
+            		        {"name":"IsClientIndividual","value":isClientIndividualValue},
+            		        {"name":"deviceAgrementType","value":deviceAgrementTypeValue}
+            		       ];
+            	}else{
+            		return configNameLookup[input];
+            	}
+            };
         }
     });
     mifosX.ng.application.filter('ConfigLookup', ['$http',mifosX.filters.ConfigLookup]).run(function($log) {
