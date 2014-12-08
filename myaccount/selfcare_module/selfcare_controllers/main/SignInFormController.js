cc0a734aebe702dc47e825ab571a738e13aa6649
mujeeb1238 <mujeeb1238@gmail.com>
2014-12-08 17:18:49 +0530

    selfcare changes

--- myaccount/selfcare_module/selfcare_controllers/main/SignInFormController.js
+++ myaccount/selfcare_module/selfcare_controllers/main/SignInFormController.js
@@ -1,38 +1,56 @@
 (function(selfcare_module) {
   selfcare.controllers = _.extend(selfcare_module, {
-	  SignInFormController: function(scope,webStorage,authenticationService,rootScope) {
-		  rootScope.loginCredentials = {};
-		  rootScope.authenticationFailed = false;
-		  rootScope.signInProcessLoading = false;
-		  rootScope.emptyCredentials = false;
+	  SignInFormController: function(scope,webStorage,RequestSender,authenticationService,rootScope,location) {
+		  
+		  scope.loginCredentials = {};
+		  scope.isProcessing  = false;
 	      
 	      scope.login = function() {
-	    	  rootScope.authenticationFailed = false;
-	    	  rootScope.isChangePassword = false;
-	    	  if(rootScope.loginCredentials.username&&rootScope.loginCredentials.password){
-	    		  rootScope.signInProcessLoading = true;
-	    		  rootScope.emptyCredentials = false;
-	    		  webStorage.add("loginCredentials",rootScope.loginCredentials);
-	    		  authenticationService.authenticateWithUsernamePassword(rootScope.loginCredentials);
+	    	  
+	    	  rootScope.signupErrorMsgs = [];rootScope.loginErrorMsgs = [];rootScope.infoMsgs = [];
+	    	  
+	    	  if(scope.loginCredentials.username && scope.loginCredentials.password){
+	    		  
+	    		  authenticationService.authenticateWithUsernamePassword(function(data){
+	    			  
+	    			  scope.isProcessing = true;
+	    			  RequestSender.loginUser.save(scope.loginCredentials,function(successData){
+	    				  webStorage.add("selfcare_sessionData", {userId: data.userId, authenticationKey: data.base64EncodedAuthenticationKey});
+	    				  webStorage.add("clientTotalData", successData);
+	    				  rootScope.currentSession= {user :successData.clientData.displayName||"abc"};
+	    				  rootScope.isSignInProcess = true;
+	    				  scope.loginCredentials = {};rootScope.signUpCredentials = {};
+	            		  rootScope.signupErrorMsgs  =[];rootScope.loginErrorMsgs  =[];rootScope.infoMsgs  =[];
+	        	    	  scope.isProcessing  = false;
+	            		  location.path('/profile');
+	            	  },function(errorData){
+	            		  rootScope.loginErrorMsgs.push({'name' : 'error.login.failed'});
+	            		  scope.isProcessing = false;
+	            	  });
+	    		  });
 	    	  }else {
-	    		  rootScope.emptyCredentials = true;
+	    		  rootScope.loginErrorMsgs.push({'name' : 'title.fill.alldetails'});
 	    	  }
 	        
 	      };
+	      
+	      
 	      $('#pwd').keypress(function(e) {
 	          if(e.which == 13) {
 	              scope.login();
 	          }
 	        });
-	      rootScope.$on("UserAuthenticationFailureEvent", function(data) {
-	    	  rootScope.authenticationFailed = true;
-	    	  rootScope.signInProcessLoading = false;
-	    	  rootScope.emptyCredentials = false;
-	      });
 		  
     }
   });
-  selfcare.ng.application.controller('SignInFormController', ['$scope','webStorage','AuthenticationService','$rootScope', selfcare.controllers.SignInFormController]).run(function($log) {
+  selfcare.ng.application.controller('SignInFormController', [
+                                                              '$scope',
+                                                              'webStorage',
+                                                              'RequestSender',
+                                                              'AuthenticationService',
+                                                              '$rootScope', 
+                                                              '$location', 
+                                                              selfcare.controllers.SignInFormController]).run(function($log) {
       $log.info("SignInFormController initialized");
   });
 }(selfcare.controllers || {}));
