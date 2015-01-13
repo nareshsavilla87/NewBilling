fabf0d550624362cf996ef8564d0f776e0316429
Rakesh <rakesh.adlagatta@gmail.com>
2015-01-13 20:06:17 +0530

    voucher changes

--- app/scripts/controllers/organization/VoucherpinController.js
+++ app/scripts/controllers/organization/VoucherpinController.js
@@ -1,11 +1,17 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-	  VoucherpinController: function(scope, resourceFactory,PermissionService,rootScope,API_VERSION,route) {
+	  VoucherpinController: function(scope, resourceFactory,PermissionService,rootScope,API_VERSION,route,paginatorService,$modal) {
         scope.voucherpins = [];
         scope.PermissionService = PermissionService;
-        resourceFactory.voucherpinResource.getAllEmployees(function(data) {
+       
+        scope.voucherPinFetchFunction = function(offset, limit, callback) {
+			resourceFactory.voucherpinResource.get({offset: offset, limit: limit} , callback);
+		};
+		
+		scope.voucherpins = paginatorService.paginate(scope.voucherPinFetchFunction, 14);
+      /*  resourceFactory.voucherpinResource.getAllEmployees(function(data) {
             scope.voucherpins = data;
-        });
+        });*/
         
          scope.downloadFile = function (id){
         	window.open(rootScope.hostUrl+ API_VERSION +'/vouchers/'+id+'?tenantIdentifier=default');
@@ -26,6 +32,38 @@
          };
         };
         
+      //export Batch 
+		scope.exportBatch = function(value){
+			scope.isProcessedValue = value;
+			$modal.open({
+				templateUrl: 'downloadBatchData.html',
+				controller: DownloadBatchDataController,
+				resolve:{}
+			});
+		};
+		var DownloadBatchDataController = function($scope, $modalInstance){
+			
+				$scope.batchDatas = {};
+				$scope.isProcessedValue = scope.isProcessedValue;
+				
+				resourceFactory.voucherpinBatchTemplateResource.query({isProcessed:$scope.isProcessedValue},function(data) {
+					$scope.batchDatas = data;
+			    });
+				
+				$scope.accept = function(id){
+					if($scope.isProcessedValue == 'true'){
+						scope.downloadFile(id);
+					}else{
+						scope.processFile(id);
+					}
+					$modalInstance.close('delete');
+				};
+		
+				$scope.reject = function(){
+					$modalInstance.dismiss('cancel');
+				};
+			};	
+        
     }
   
   });
@@ -36,6 +74,8 @@
                                                             '$rootScope',
                                                             'API_VERSION',
                                                             '$route',
+                                                            'PaginatorService',
+                                                            '$modal',
                                                             mifosX.controllers.VoucherpinController]).run(function($log) {
 	  
     $log.info("VoucherpinController initialized");
