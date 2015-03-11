78178a7a938470bcb455687a1e42545bf66c8fe6
ranjith <ranjith@ranjith.(none)>
2015-03-11 12:11:28 +0530

    edit radService

---
+++ app/scripts/controllers/organization/EditRadServiceController.js
@@ -0,0 +1,56 @@
+(function(module) {
+	mifosX.controllers = _.extend(module, {
+		EditRadServiceController : function(scope, location,  $modal, route,$http, webStorage,resourceFactory,routeParams) {
+			
+		
+			scope.radiusVersion = routeParams.radiusVersion;
+			  
+			scope.types =[{key:'K' ,value:"KB"},{key:'M',value:"MB"},{key:'G',value:"GB"}];
+			scope.formData = {				 
+					upType  : scope.types[0].key,
+					downType  : scope.types[0].key,	  
+			};
+			  
+			 scope.reset123 = function(){
+	        	   webStorage.add("callingTab", {someString: "radService" });
+	           };
+	           
+	           if(scope.radiusVersion == 'version-2'){
+	        	   scope.serviceCodes = [];
+	        	   resourceFactory.radServiceTemplateResource.query(function(data) {
+	        		   scope.serviceCodes = data;
+	               });
+	           }
+			
+			scope.submit = function() {
+				if(scope.radiusVersion == 'version-1'){
+					scope.formData.value=scope.formData.downRate + scope.formData.downType +"/"+ scope.formData.upRate + scope.formData.upType;
+					delete scope.formData.downRate ;
+					delete scope.formData.upRate;
+				}else if(scope.radiusVersion == 'version-2'){
+					delete scope.formData.upType;
+					delete scope.formData.downType;
+				}
+					//console.log(scope.formData.value);
+			    resourceFactory.radServiceResource.save(scope.formData,function(data){
+			        		  location.path('/radius/' );
+			          });
+			    webStorage.add("callingTab", {someString: "radService" });
+				
+		  };
+		}
+	});
+	mifosX.ng.application.controller('EditRadServiceController',[ 
+	    '$scope',
+	    '$location',
+	    '$modal',
+	    '$route',
+	    '$http',
+	    'webStorage',
+	    'ResourceFactory',
+	    '$routeParams',
+	    mifosX.controllers.EditRadServiceController 
+	    ]).run(function($log) {
+	    	$log.info("EditRadServiceController initialized");
+	    });
+}(mifosX.controllers || {}));
