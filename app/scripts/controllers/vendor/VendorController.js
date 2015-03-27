640c26faae886c99549cdaa2fbd1c738def48890
Rakesh <rakesh.adlagatta@gmail.com>
2015-03-27 12:02:58 +0530

    vendormanagement

--- app/scripts/controllers/vendor/VendorController.js
+++ app/scripts/controllers/vendor/VendorController.js
@@ -11,6 +11,30 @@
         	location.path('/viewvendormanagement/' +id);
         };
         
+        /**
+       	 * Delete Vendor
+       	 * */
+         scope.deleteVendor = function (id){
+         	scope.vendorId=id;
+          	 $modal.open({
+  	                templateUrl: 'deletePopupForVendor.html',
+  	                controller: approve,
+  	                resolve:{}
+  	        });
+         };
+          
+      	function  approve($scope, $modalInstance) {
+      		$scope.approve = function () {
+              	resourceFactory.VendorLemplateResource.remove({vendorId: scope.vendorId} , {} , function(data) {
+                    route.reload();
+              	});
+              	$modalInstance.dismiss('delete');
+      		};
+            $scope.cancel = function () {
+                  $modalInstance.dismiss('cancel');
+            };
+        }
+        
     }
   });
   mifosX.ng.application.controller('VendorController', [
--- app/scripts/controllers/vendor/ViewVendorController.js
+++ app/scripts/controllers/vendor/ViewVendorController.js
@@ -1,16 +1,40 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-	  ViewVendorController: function(scope, routeParams , resourceFactory ,location,$modal,PermissionService) {
+	  ViewVendorController: function(scope, routeParams , resourceFactory ,location,$modal,PermissionService,route) {
         scope.vendorData = [];
         scope.PermissionService = PermissionService;
-       
+        scope.vendorRouteParamId = routeParams.id;
         resourceFactory.VendorLemplateResource.getTemplateDetails({vendorId: routeParams.id} , function(data) {
         	scope.vendorData = data;
         });
         
+        /**
+       	 * Delete Vendor
+       	 * */
+         scope.deleteVendor = function (id){
+         	scope.vendorId=id;
+          	 $modal.open({
+  	                templateUrl: 'deletePopupForVendor.html',
+  	                controller: approve,
+  	                resolve:{}
+  	        });
+         };
+          
+      	function  approve($scope, $modalInstance) {
+      		$scope.approve = function () {
+              	resourceFactory.VendorLemplateResource.remove({vendorId: scope.vendorId} , {} , function(data) {
+              		location.path('/vendormanagement');
+              	});
+              	$modalInstance.dismiss('delete');
+      		};
+            $scope.cancel = function () {
+                  $modalInstance.dismiss('cancel');
+            };
+        }
+        
     }
   });
-  mifosX.ng.application.controller('ViewVendorController', ['$scope', '$routeParams','ResourceFactory', '$location','$modal','PermissionService',mifosX.controllers.ViewVendorController]).run(function($log) {
+  mifosX.ng.application.controller('ViewVendorController', ['$scope', '$routeParams','ResourceFactory', '$location','$modal','PermissionService','$route',mifosX.controllers.ViewVendorController]).run(function($log) {
     $log.info("ViewVendorController initialized");
   });
 }(mifosX.controllers || {}));
