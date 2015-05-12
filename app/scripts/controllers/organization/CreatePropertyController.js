6a5e08a66cbf4a6b7ed572105ec162553ee5527a
ranjith15 <ranjithworld150@gmail.com>
2015-05-12 15:43:53 +0530

    ..

--- app/scripts/controllers/organization/CreatePropertyController.js
+++ app/scripts/controllers/organization/CreatePropertyController.js
@@ -4,6 +4,10 @@
 	
 			scope.propertyTypes = [];
 			scope.precinctData = [];
+			scope.parcelData = [];
+			scope.buildingData = [];
+			scope.floorData = [];
+			scope.unitData =[];
 			scope.formData = {};
 		
 			scope.PermissionService = PermissionService;
@@ -29,10 +33,10 @@
 				if(precinct!=undefined){
 				    for(var i in scope.precinctData){
 				    	if(precinct==scope.precinctData[i].cityName){
-				    		scope.precinct = scope.precinctData[i].cityCode.substr(0,2);
+				    		scope.precinctCode = scope.precinctData[i].cityCode.substr(0,2);
 			          		scope.formData.state =  scope.precinctData[i].state;
 			          		scope.formData.country = scope.precinctData[i].country;
-			          	//	scope.formData.cityName=scope.precinctData[i].cityName;
+			          		scope.getWatch(scope.precinctCode);
 			          		break;
 			          }else{
 			        	   
@@ -41,19 +45,123 @@
 						}
 					}
 				  }else{
-					  //  delete scope.formData.propertyCode;
+					  
 						delete scope.formData.state;
 			    		delete scope.formData.country; 
 				  }
 			};
-			
-			scope.getPropertyCode=function(unitCode){
-				if(scope.precinct !=undefined&&scope.formData.parcel!=undefined&&scope.formData.buildingCode!=undefined && scope.formData.floor!=undefined){
-			    scope.formData.propertyCode=scope.precinct.concat(scope.formData.parcel,scope.formData.buildingCode,scope.formData.floor,unitCode);
+			  
+				scope.getParcel = function(queryParam){
+					return http.get($rootScope.hostUrl+API_VERSION+'/propertymaster/type/', {
+		        	      params: {
+		        	    	  		query: 'parcel',
+		        	    	  		queryParam:queryParam		
+		        	      		   }
+		        	    }).then(function(res){   
+		        	    	 scope.parcelData=res.data;	
+		        	      return scope.parcelData;
+		        	    });
+	             };   
+	             
+	             scope.getParcelDetails = function(parcel){
+	            	 if(parcel !=undefined){
+	                 for(var i in scope.parcelData){
+	                	 if(parcel== scope.parcelData[i].description){
+					    		scope.parcel = scope.parcelData[i].code.substr(0,2);
+					    		scope.formData.street = scope.parcelData[i].referenceValue;
+					    		scope.getWatch(scope.parcel);
+				          		break;
+				          }	 
+	                    }
+	                }
+	             };
+	             
+	             scope.getBuild = function(queryParam){
+						return http.get($rootScope.hostUrl+API_VERSION+'/propertymaster/type/', {
+			        	      params: {
+			        	    	  		query: 'Building Codes',
+			        	    	  		queryParam:queryParam				
+			        	      		   }
+			        	    }).then(function(res){   
+			        	    	 scope.buildingData=res.data;	
+			        	      return scope.buildingData;
+			        	    });
+		             };  
+		             
+		             scope.getbuildCode = function(buildingCode){
+		            	 if(!angular.isUndefined(buildingCode)){
+						    		scope.formData.buildingCode = buildingCode.substr(0,3);
+						    		scope.getWatch(scope.formData.buildingCode);
+					          }	 
+		             };  
+	             
+				
+				scope.getFloor = function(queryParam){
+					return http.get($rootScope.hostUrl+API_VERSION+'/propertymaster/type/', {
+		        	      params: {
+		        	    	  		query: 'Level/Floor',
+		        	    	  		queryParam:queryParam	
+		        	      		   }
+		        	    }).then(function(res){   
+		        	    	 scope.floorData=res.data;	
+		        	      return scope.floorData;
+		        	    });
+	             };   
+	             
+	             scope.getFloorDetails = function(floor){
+	            	 if(floor!=undefined){
+	            		 for( var i in scope.floorData){
+	            			 if(floor==scope.floorData[i].description){
+						    		scope.floor = scope.floorData[i].code.substr(0,2);
+						    	    scope.getWatch(scope.floor);
+					          		break;
+					          }	 
+		                 }
+	            	 }	       	        
+	          };
+	          
+	          
+	          scope.getUnit = function(queryParam){
+					return http.get($rootScope.hostUrl+API_VERSION+'/propertymaster/type/', {
+		        	      params: {
+		        	    	  		query: 'Unit Codes',
+		        	    	  		queryParam:queryParam		
+		        	      		   }
+		        	    }).then(function(res){   
+		        	    	 scope.unitData=res.data;	
+		        	      return scope.unitData;
+		        	    });
+	             };   
+	             
+	             scope.getunitCode = function(unit){
+	            	 if(!angular.isUndefined(unit)){
+	                	 scope.formData.unitCode=unit.substr(0,4);
+	                	 console.log(scope.formData.propertyCode);
+	                	if(angular.isUndefined(scope.formData.propertyCode)){
+	                	      scope.getPropertyCode(scope.formData.unitCode);
+	                	}else{
+	                		scope.getWatch(scope.formData.unitCode);
+	                	   }					 
+				      }	 
+	             };
+	          
+	  		scope.getPropertyCode=function(unitCode){
+				if(scope.precinctCode !=undefined&&scope.parcel!=undefined&&scope.formData.buildingCode!=undefined &&scope.floor!=undefined){
+			     scope.formData.propertyCode=scope.precinctCode.concat(scope.parcel,scope.formData.buildingCode,scope.floor,unitCode);
+			    // scope.getProperty($scope.formData.propertyCode);
 				}
+			}; 
+			
+			 scope.getWatch=function(labelValue){
+				if(!angular.isUndefined(labelValue)&&!angular.isUndefined(scope.formData.propertyCode)){
+					scope.formData.propertyCode=scope.precinctCode.concat(scope.parcel,scope.formData.buildingCode,scope.floor,scope.formData.unitCode);
+				}			
+					
 			};
+
 			scope.submit = function() { 
-				console.log(scope.formData.precinct);
+				scope.formData.parcel=scope.parcel;
+				scope.formData.floor=scope.floor;
 				resourceFactory.propertyCodeResource.save(scope.formData,function(data){
 				location.path('/viewproperty/'+data.resourceId);
 				});
