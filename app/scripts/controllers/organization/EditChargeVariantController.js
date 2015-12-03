3d6b5a32ce006b4fb98d7ebd76bb454cddce7733
rakesh <rakesh.adlagatta@gmail.com>
2015-12-03 16:27:20 +0530

    edit chargevariant

--- app/scripts/controllers/organization/EditChargeVariantController.js
+++ app/scripts/controllers/organization/EditChargeVariantController.js
@@ -1,8 +1,8 @@
 (function(module) {
 	mifosX.controllers = _.extend(module, {
 		EditChargeVariantController : function(scope, routeParams,resourceFactory, location, $rootScope) {
-
-			    resourceFactory.chargevariantResource.get({chargeVarintId : routeParams.id,	template : 'true'}, function(data) {
+alert(routeParams.id);
+			    resourceFactory.chargevariantResource.get({chargeVariantId : routeParams.id, template : 'true'}, function(data) {
 				scope.formData = data;
 				scope.chargeVarintId = routeParams.id;
 
